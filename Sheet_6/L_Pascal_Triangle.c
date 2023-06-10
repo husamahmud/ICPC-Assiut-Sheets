88c4bc0628ecc6be243abef9c6501c76cf59337b
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-10 18:56:45 +0300

    A->P

--- Sheet_6/L_Pascal_Triangle.c
+++ Sheet_6/L_Pascal_Triangle.c
@@ -5,8 +5,6 @@ int main() {
     scanf("%d", &n);
 
     int triangle[n][n];
-
-    // Initialize the triangle with all elements as 1
     /*
     [0][0]
     [1][0]  [1][1]
@@ -14,6 +12,8 @@ int main() {
     [3][0]  [3][1]  [3][2]  [3][3]
     ....
     */
+
+    // Initialize the triangle with all elements as 1
     for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) {
             if (j == 0 || j == i) {
