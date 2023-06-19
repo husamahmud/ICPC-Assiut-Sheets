60c61ae1e12a072facfdaec725afc2bbb1d135a2
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-19 14:39:52 +0300

    Sheet #7

--- Sheet_7/Z_Left_Max.c
+++ Sheet_7/Z_Left_Max.c
@@ -1,1 +1,30 @@
 #include<stdio.h>
+long long maxVal;
+int n;
+long long current;
+
+void leftMax(int ctr){
+    if(ctr == n)
+        return;
+
+    scanf("%lld", &current);
+
+    if(ctr == 0){
+        printf("%lld ", current);
+        maxVal = current;
+    } else {
+        if (current > maxVal){
+            printf("%lld ", current);
+            maxVal = current;
+        }
+        else
+            printf("%lld ", maxVal);
+    }
+    return leftMax(ctr+1);
+}
+
+
+int main(void){
+    scanf("%d", &n);
+    leftMax(0);
+}
