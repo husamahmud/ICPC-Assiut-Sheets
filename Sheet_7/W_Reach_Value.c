ffed32d76525cfdc29878f838491e3ff7dc3d94d
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2023-06-19 13:21:40 +0300

    W

--- Sheet_7/W_Reach_Value.c
+++ Sheet_7/W_Reach_Value.c
@@ -1,1 +1,24 @@
 #include<stdio.h>
+#include<stdbool.h>
+unsigned long long x;
+
+
+bool check(unsigned long long t){
+    if (t > x)
+        return false;
+    else if (t == x)
+        return true;
+    else
+        return check(t * 10) || check(t * 20);
+}
+
+int main(void){
+    long long n;
+    scanf("%lld", &n);
+    for(int i = 0; i<n; i++)
+    {
+        scanf("%llu", &x);
+        check(1) ? printf("YES\n") : printf("NO\n");
+    }
+
+}
